abd3e7334428aaaead84711322a7b87b89365e73
nguyenvmthien <114507572+nguyenvmthien@users.noreply.github.com>
2023-12-14 22:45:13 +0700

    Update colors.cpp

--- src/colors.cpp
+++ src/colors.cpp
@@ -1,17 +1,18 @@
 #include "colors.h"
 
 const Color darkGrey = {26, 31, 40, 255};
-const Color green = {47, 230, 23, 255};
-const Color red = {232, 18, 18, 255};
-const Color orange = {226, 116, 17, 255};
-const Color yellow = {237, 234, 4, 255};
-const Color purple = {166, 0, 247, 255};
-const Color cyan = {21, 204, 209, 255};
+const Color green = {112,195,121,255};
+const Color red = {255, 0, 0, 255};
+const Color orange = {255, 107, 1, 255};
+const Color yellow = {255, 214, 0, 255};
+const Color purple = {148, 82, 255, 255};
+const Color cyan = {30, 181, 218, 255};
 const Color blue = {13, 64, 216, 255};
 const Color lightBlue = {59, 85, 162, 255};
 const Color darkBlue = {44, 44, 127, 255};
+const Color white = {255, 255, 255, 255};
 
 std::vector<Color> GetCellColors()
 {
-    return {darkGrey, green, red, orange, yellow, purple, cyan, blue};
+    return {darkGrey, green, red, orange, yellow, purple, cyan, white};
 }
